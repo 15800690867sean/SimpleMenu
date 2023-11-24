31d759dc0af9ce3a1c525e1079a0e7ff14e9ca04
唐肖恩 <15800690867@163.com>
2023-11-24 18:58:23 +1100

    feat: finish the layout of default page

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React, { Component } from 'react';
+import './index.css';
+
+export default class page extends Component {
+  render() {
+    return (
+      <div className='page'>page</div>
+    )
+  }
+};
