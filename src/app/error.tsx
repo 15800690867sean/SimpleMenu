350b8e1293f81a26e7b76f86b0229841e1a72c04
唐肖恩 <15800690867@163.com>
2023-12-19 19:17:22 +1100

    feat: config the prettier

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import React from 'react'
+import React from "react";
 
 export default function Error({
-    error,
-    reset
-} : {
-    error: Error,
-    reset: () => void,
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
 }) {
   return (
     <div>
-        <div>There is an error!</div>
-        <div>{error.message}</div>
-        <button onClick={() => reset()}>Reset</button>
+      <div>There is an error!</div>
+      <div>{error.message}</div>
+      <button onClick={() => reset()}>Reset</button>
     </div>
-  )
+  );
 }
